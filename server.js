71e16f5ce4755eedfb2876f345723557708b9099
12kenzie <riguermackenzie155@gmail.com>
2026-01-17 08:41:28 +0800

    Remove logger

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const telegramBot = require('./telegramBot');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.listen(3000, () => console.log('Server running on port 3000'));
+
+
 // Firebase Admin SDK
 const admin = require("firebase-admin");
 
